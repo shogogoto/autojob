022d9168bd332c138103bdc1ba7402926359e53f
Shogo goto <gotoadmn0605@gmail.com>
2020-03-15 23:20:27 +0900

    contextのコントロール実装

--- autojob/progress/progress.py
+++ autojob/progress/progress.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from contextlib import ExitStack
 
 
 class Progress:
@@ -6,7 +7,9 @@ class Progress:
         self._case    = case
         self._current = None 
         self._processes = []
-    
+        self._contexts  = []
+        self._stack = ExitStack()
+
     @property
     def case(self):
         return self._case
@@ -16,20 +19,27 @@ class Progress:
         return len(self._processes)
 
     # decorator
-    def add_proc(self, proc_func):
-        # proc_func: (Progress) -> Job, checker
-        # これをテストで定義すべし
-        def wrapper(progress):
-            job, checker = proc_func(progress)
-            if self._current is None:
-                state = checker.check(job)
-            else:
-                state = self._current.execute(job, checker)
-            self._current = state
-        self._processes.append(wrapper)
+    def add_proc(self, cm_func=None, *args, **kwargs):
+        self._contexts.append((cm_func, args, kwargs))
+        def _add_proc(proc_func):
+            # proc_func: (Progress) -> Job, checker
+            # これをテストで定義すべし
+            def wrapper(progress):
+                job, checker = proc_func(progress)
+                if self._current is None:
+                    state = checker.check(job)
+                else:
+                    state = self._current.execute(job, checker)
+                self._current = state
+            self._processes.append(wrapper)
+        return _add_proc
 
     def advance(self):
         for _ in iter(self._case):
-            for proc in self._processes:
+            for i, proc in enumerate(self._processes):
+                cm_func, args, kwargs = self._contexts[i]
+                if cm_func is not None:
+                    self._stack.enter_context(cm_func(*args, **kwargs))
                 proc(self)
             self._current = None
+            self._stack.close()
--- tests/progress/test_progress.py
+++ tests/progress/test_progress.py
@@ -1,6 +1,7 @@
 import unittest
 from pathlib import Path
 import os
+import shutil
 from contextlib import contextmanager
 from autojob.progress import Progress
 
@@ -29,26 +30,41 @@ class TestProgress(unittest.TestCase):
     def setUp(self):
         self.prog = Progress("abcd")
         
-        @self.prog.add_proc
+        @self.prog.add_proc(chdir, "proc1")
         def test_proc1(progress):
-            with chdir("proc1"):
-                print(os.getcwd())
-                progress.test_state = 0
-                return "dummy_job", TestChecker()
+            progress.test_state = 0
+            print("proc1", os.getcwd())
+            return "dummy_job", TestChecker()
         
-        @self.prog.add_proc
+        @self.prog.add_proc(chdir, "proc2")
         def test_proc2(progress):
-            with chdir("proc2"):
-                #print(progress.test_state)
-                return "dummy_job", TestChecker()
+            print("prco2", os.getcwd())
+            return "dummy_job", TestChecker()
 
     def test_n_proc(self):
         self.assertEqual(self.prog.n_proc, 2)
 
     def test_advance(self):
-        print(os.getcwd())
         self.prog.advance()
-        print(os.getcwd())
         self.assertTrue(os.path.exists("proc1/proc2"))
+        shutil.rmtree("proc1")
+
+    def test_init(self):
+        @self.prog.add_proc(chdir, "a")
+        def test_case_a(progress):
+            return "dummy_job", TestChecker()
+        
+        @self.prog.add_proc(chdir, "b")
+        def test_case_b(progress):
+            return "dummy_job", TestChecker()
+        
+        @self.prog.add_proc(chdir, "c")
+        def test_case_c(progress):
+            return "dummy_job", TestChecker()
+
+        self.prog.advance()
+        self.assertTrue(os.path.exists("proc1/proc2/a/b/c"))
+        shutil.rmtree("proc1")
+
 
 
